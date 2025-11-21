40ec2fe460089f791f83edf51430598b81ecd660
viswanathan <vishwa.graphic@gmail.com>
2025-11-20 18:07:35 -0600

    changes for husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage    ] = useState('');
 
   useEffect(() => {
     fetch('/api/hello')
       .then((res) => res.json())
-           .then((data) => {
-        setMessage(data.message)
+          .then((data) => {
+        setMessage(data.message);
       });
   }, []);
 
   return (
     <div className="p-4">
-      <p className="font-bold text qw-3xl">{message}</p>
+            <p className="font-bold text qw-3xl">{message}</p>
       <Button>Click me !</Button>
     </div>
   );
