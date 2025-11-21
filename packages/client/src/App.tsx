077d86f3a7e9f7b3673c2e54e51a606fccba3725
viswanathan <vishwa.graphic@gmail.com>
2025-11-20 18:03:27 -0600

    changes for husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="p-4">
-           <p className="font-bold text-3xl">{message}</p>
+          <p className="font-bold text-3xl">{message}</p>
       <Button>Click me !</Button>
     </div>
   );
